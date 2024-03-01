7940621559bf456f348efdfd5b9b4f1b42509172
AndreSebastiao <andresebastiao755@gmail.com>
2024-03-01 23:41:24 +0000

    Byte values

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Values
+            byte    bt;
+
+            bt = byte.MaxValue;
+            Console.WriteLine(bt);
+            bt = byte.MinValue;
+            Console.WriteLine(bt);
+        }
+    }
+}
