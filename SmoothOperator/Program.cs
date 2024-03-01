c95e7baba5c5e2d75ac95024fb5dc5c2e0f2bd6f
AndreSebastiao <andresebastiao755@gmail.com>
2024-03-01 23:02:11 +0000

    Turn number into byte and increment and decrement said number

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asking the user for a positive whole number
+            Console.Write("Insert a positive whole number: ");
+
+            //Converting the number in to a byte
+            byte n = byte.Parse(Console.ReadLine());
+
+            //Output the byte with increments and decrements
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+        }
+    }
+}
