7307183f05a13ab6d17c7b99c1b0f0f6f34b3b15
AndreSebastiao <andresebastiao755@gmail.com>
2024-03-01 23:19:30 +0000

    Get and Transform Hight and Radius in to a float

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,7 +6,16 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Ask for Hight
+            Console.Write("High of the Cylinder: ");
+            //Read and Transform Hight in to a float
+            float a = float.Parse(Console.ReadLine());
+            //Ask for Radius
+            Console.Write("Radius of the Cylinder: ");
+            //Read and Transform Radius in to a float
+            float r = float.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.WriteLine(r);
         }
     }
 }
