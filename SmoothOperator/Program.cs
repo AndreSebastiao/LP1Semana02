a1632c81abc0f9fa9bb7fc87648103582600c21b
AndreSebastiao <andresebastiao755@gmail.com>
2025-02-26 14:33:36 +0000

    exercicio 1

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insere um número inteiro: ");
+            sbyte i = sbyte.Parse(Console.ReadLine());
+
+            i--;
+            Console.WriteLine(i);
+
+            i++;
+            Console.WriteLine(i);
+        }
+    }
+}
