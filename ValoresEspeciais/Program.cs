d804215bcb3b51de7423521c643aea1a0e3eb0cc
AndreSebastiao <andresebastiao755@gmail.com>
2025-02-26 15:35:44 +0000

    exercicio 6

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,55 @@ namespace ValoresEspeciais
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            byte    bt;
+            sbyte   sbt;
+            short   sh;
+            ushort  ush;
+            int     i;
+            uint    ui;
+            long    ln;
+            ulong   uln;
+            char    ch;
+            double  db;
+            float   f;
+            float   f2;
+            decimal dc;
+
+            Console.WriteLine(bt = byte.MaxValue);
+            Console.WriteLine(bt = byte.MinValue);
+
+            Console.WriteLine(sbt = sbyte.MaxValue);
+            Console.WriteLine(sbt = sbyte.MaxValue);
+
+            Console.WriteLine(sh = short.MaxValue);
+            Console.WriteLine(sh = short.MinValue);
+
+            Console.WriteLine(ush = ushort.MaxValue);
+            Console.WriteLine(ush = ushort.MinValue);
+
+            Console.WriteLine(i = int.MaxValue);
+            Console.WriteLine(i = int.MinValue);
+
+            Console.WriteLine(ui = uint.MaxValue);
+            Console.WriteLine(ui = uint.MinValue);
+
+            Console.WriteLine(ln = long.MaxValue);
+            Console.WriteLine(ln = long.MinValue);
+
+            Console.WriteLine(uln = ulong.MaxValue);
+            Console.WriteLine(uln = ulong.MinValue);
+
+            Console.WriteLine(ch = char.MaxValue);
+            Console.WriteLine(ch = char.MinValue);
+
+            Console.WriteLine(db = double.MaxValue);
+            Console.WriteLine(db = double.MinValue);
+
+            Console.WriteLine(f = float.MaxValue);
+            Console.WriteLine(f = float.MinValue);
+
+            Console.WriteLine(dc = decimal.MaxValue);
+            Console.WriteLine(dc = decimal.MinValue);
         }
     }
 }
