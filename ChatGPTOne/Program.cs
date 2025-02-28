eefe722db59702201864ab2aa925d7ecb98c1992
AndreSebastiao <andresebastiao755@gmail.com>
2025-02-28 14:43:04 +0000

    exercício de avaliação

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Start number: ");
+                int i = int.Parse(Console.ReadLine());
+
+                Console.Write("Step number: ");
+                int j = int.Parse(Console.ReadLine());
+
+                if (i < j)
+                {
+                    Console.WriteLine("Start must be higher than step. Try again.");
+                    continue;
+                }
+                else if (i % j == 0)
+                {
+                    Console.WriteLine("Start not divisible by step. Try again.");
+                    continue;
+                }
+                else
+                {
+                    do
+                    {
+                        Console.WriteLine(i);
+                    } 
+                    while (i > 0);
+                }
+
+            }
+        }
+    }
+}
---
+++ PerfectCountdown/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PerfectCountdown
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Start number: ");
+                int i = int.Parse(Console.ReadLine());
+
+                Console.Write("Step number: ");
+                int j = int.Parse(Console.ReadLine());
+
+                if (i <= 0)
+                {
+                    Console.WriteLine("Out-of-range start or step. Try again.");
+                    continue;
+                }
+                else if (i <= j)
+                {
+                    Console.WriteLine("Start must be higher than step. Try again.");
+                    continue;
+                }
+                else if (i % j != 0)
+                {
+                    Console.WriteLine("Start not divisible by step. Try again.");
+                    continue;
+                }
+                else
+                {
+                    while (true)
+                    {
+                        Console.WriteLine(i);
+                        if (i == 0)
+                        {
+                            break;
+                        }
+                        i = i - j;
+                    }                  
+                }
+            }
+        }
+    }
+}
